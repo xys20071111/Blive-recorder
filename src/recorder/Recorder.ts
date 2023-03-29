e571f0b46c5727e425d34e357e2c1534bc7bcb4a
xys20071111 <xys20071111@hotmail.com>
2023-03-30 02:53:06 +0800

    开始写m3u8处理器

--- src/recorder/Recorder.ts
+++ src/recorder/Recorder.ts
@@ -1,37 +1,36 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { WriteStream, createWriteStream } from 'fs'
 import https from 'https'
+import { mkdirSync } from 'fs'
 import { EventEmitter } from 'events'
-import { request, getTimeString, printLog } from '../utils'
-import { IncomingMessage } from 'http'
-import { spawn } from 'child_process'
+import { request, getTimeString } from '../utils'
+import { response } from 'express'
 
 
 class Recorder extends EventEmitter {
 	private roomId: number
 	private outputPath: string
 	private outputFile: string
-	private outputStream?: WriteStream
-	private isFirstMeta = true
-	private isFirstHeader = true
-	private lastClipA = new Set<number>()
-	private lastClipV = new Set<number>()
+	private clipDir: string
+	private outputFileStream: WriteStream
+	private clipList: Array<string> = []
 
 	constructor(roomId: number, outputPath: string) {
 		super()
 		this.roomId = roomId
 		this.outputPath = outputPath
-		this.outputFile = `${outputPath}/${getTimeString()}.flv`
+		this.outputFile = `${outputPath}/${getTimeString()}.m3u8`
+		this.outputFileStream = createWriteStream(this.outputFile)
+		this.clipDir = this.outputFile.replace('.m3u8', '/')
 	}
 
 	public createFileStream() {
 		console.log('创建新文件')
-		this.outputFile = `${this.outputPath}/${getTimeString()}.flv`
-		this.outputStream = createWriteStream(this.outputFile, { flags: 'a', encoding: 'binary' })
-		this.isFirstHeader = true
-		this.isFirstMeta = true
-		this.lastClipA.clear()
-		this.lastClipV.clear()
+		this.outputFile = `${this.outputPath}/${getTimeString()}.m3u8`
+		this.outputFileStream = createWriteStream(this.outputFile)
+		this.outputFileStream.write('#EXTM3U\n#EXT-X-VERSION:7\n#EXT-X-START:TIME-OFFSET=0\n#EXT-X-MEDIA-SEQUENCE:39148412\n#EXT-X-TARGETDURATION:1\n')
+		this.clipDir = this.outputFile.replace('.m3u8', '/')
+		mkdirSync(this.clipDir)
 	}
 
 	async start(isNewLive: boolean) {
@@ -39,19 +38,19 @@ class Recorder extends EventEmitter {
 			this.createFileStream()
 		}
 		const data = (await request('/xlive/web-room/v2/index/getRoomPlayInfo', 'GET', {
-			room_id:this.roomId,
-			no_playurl:0,
-			mask:1,
-			qn:	10000,
-			platform:'web',
-			protocol:'0,1',
-			format:'0,2',
-			codec:'0,1'
+			room_id: this.roomId,
+			no_playurl: 0,
+			mask: 1,
+			qn: 10000,
+			platform: 'web',
+			protocol: '0,1',
+			format: '0,2',
+			codec: '0,1'
 		})).data
 		let streamHost: string = '';
 		let streamParma: string = '';
-		let streamPath: string  = '';
-		for(const item of data.playurl_info.playurl.stream) {
+		let streamPath: string = '';
+		for (const item of data.playurl_info.playurl.stream) {
 			if (item.protocol_name === 'http_hls') {
 				streamHost = item.format[0].codec[0].url_info[0].host
 				streamParma = item.format[0].codec[0].url_info[0].extra
@@ -59,19 +58,69 @@ class Recorder extends EventEmitter {
 			}
 		}
 		const streamUrl = `${streamHost}${streamPath}${streamParma}`
-		const task = spawn('ffmpeg', [
-			'-headers',
-			'Accept: */*\r\nAccept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\nAccept-Encoding: gzip, deflate, br\r\nReferer: https://live.bilibili.com/\r\nOrigin: https://live.bilibili.com\r\nDNT: 1\r\nConnection: keep-alive\r\nSec-Fetch-Dest: empty\r\nSec-Fetch-Mode: cors\r\nSec-Fetch-Site: cross-site\r\nSec-GPC: 1\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nTE: trailers',
-			'-user_agent',
-			'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
-			'-i',
-			streamUrl,
-			'-c:v', 'copy',
-			'-c:a', 'copy',
-			`${this.outputPath}/${getTimeString()}.flv`
-		])
-		task.on('exit', () => {
-			this.emit('RecordStop', 1)
+		console.log(streamUrl)
+		const recordInterval = setInterval(async () => {
+			try {
+				const m4slist = await this.getM4sList(streamUrl)
+				for (const item of m4slist) {
+					if (!this.clipList.includes(item)) {
+						const req = https.request(streamUrl.replace('index.m3u8', item), {
+							headers: {
+								'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+								Referer: 'https://live.bilibili.com/',
+								origin: 'https://live.bilibili.com/'
+							}
+						})
+						req.on('response', (res) => {
+							if (res.statusCode !== 200) {
+								return
+							}
+							const resultPath = `${this.clipDir}${item}`
+							const file = createWriteStream(resultPath)
+							res.pipe(file)
+							this.clipList.push(item)
+							this.outputFileStream.write(`#EXTINF:1.00\nfile://${resultPath}\n`)
+						})
+						req.end()
+					}
+				}
+
+			} catch {
+				this.emit('RecordStop', 1)
+				clearInterval(recordInterval)
+			}
+		}, 10000)
+	}
+	getM4sList(urlString: string): Promise<Array<string>> {
+		return new Promise((resolve, reject) => {
+			const req = https.request(urlString, {
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+					Referer: 'https://live.bilibili.com/',
+					origin: 'https://live.bilibili.com/'
+				}
+			})
+			req.on('response', (res) => {
+				if (res.statusCode !== 200 && res.statusCode !== 206) {
+					reject(res.statusCode)
+					return
+				}
+				res.setEncoding('utf-8')
+				let data = ''
+				res.on('data', chunk => data += chunk)
+				res.on('end', () => {
+					const list: Array<string> = []
+					const lines = data.split('\n')
+					for (const line of lines) {
+						if (line[0] === '#') {
+							continue
+						}
+						list.push(line)
+					}
+					resolve(list)
+				})
+			})
+			req.end()
 		})
 	}
 }
